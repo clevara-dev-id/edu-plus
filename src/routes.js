ff3b462279c44795d9fdb9486bd51dccbc9a147a
iipshoifuddin <shoifuddin.arkademy@gmail.com>
2020-03-24 20:55:54 +0700

    second commit

---
+++ src/routes.js
@@ -0,0 +1,14 @@
+import React, { lazy } from 'react'
+import { Route } from 'react-router-dom'
+
+import Home from './views/Home'
+
+const BaseRoute = () => {
+    return (
+        <div>
+            <Route exact path="/" component={Home} />
+        </div>
+    )
+}
+
+export default BaseRoute
