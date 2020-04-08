0a6283bde6c77c63189323eb5678f7c62fb813c1
iipshoifuddin <shoifuddin.arkademy@gmail.com>
2020-04-08 09:42:50 +0700

    first commit

---
+++ src/components/base_components/Description/Mobile/MobileDescription.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import './mobiledescription.css'
+
+import { Container, Row, Col } from 'react-bootstrap';
+
+const MobileDescription = props => {
+    return (
+        <>
+            <Container id="descriptionmobile">
+                {props.store.map((data)=>{
+                    return(
+                        <GroupDescription>
+                            <Row>
+                                <Col>
+                                    <TitleDescription>{data.title}</TitleDescription>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col>
+                                    <FillDescription>{data.description}</FillDescription>
+                                </Col>
+                            </Row>
+                        </GroupDescription>
+                    );
+                })}
+            </Container>
+        </>
+    );
+}
+
+const GroupDescription = styled.div`
+    margin-bottom: 16px;
+    margin-right: 15px;
+`;
+
+const TitleDescription = styled.div`
+    font-style: normal;
+    font-weight: normal;
+    font-size: 14px;
+    line-height: 17px;
+    margin-left: 15px;
+    text-align: justify;
+
+    /* identical to box height */
+    display: flex;
+    align-items: center;
+    letter-spacing: 0.001em;
+
+    /* grey light */
+    color: #B5B5B5;
+`;
+
+const FillDescription = styled.div`
+    font-style: normal;
+    font-weight: normal;
+    font-size: 14px;
+    line-height: 17px;
+    margin-left: 15px;
+    margin-top: 3px;
+    text-align: justify;
+
+    /* identical to box height */
+    display: flex;
+    align-items: center;
+    letter-spacing: 0.0025em;
+
+    /* grey dark */
+    color: #4A4A4A;
+`;
+
+MobileDescription.propTypes = {
+    store : PropTypes.object
+}
+
+export default MobileDescription;
--- src/views/DetailSecondary.js
+++ src/views/DetailSecondary.js
@@ -14,7 +14,7 @@ import KontakSekolah from '../components/base_components/Footer/Mobile/KontakSek
 import SingleBadges from '../components/base_components/Badges/BadgesMobile/SingleBadges'
 import MapsComponents from '../components/base_components/MapsComponents/Mobile/MapsComopnets';
 import LineComponents from '../components/base_components/LineComponents/Mobile/LineComponents';
-
+import MobileDescription from '../components/base_components/Description/Mobile/MobileDescription'
 
 //Import Image
 import slideImage1 from '../components/asset/images/CarouselSlider/slider1.png'
@@ -80,11 +80,46 @@ const store8 = {
     thirdCost   :"Rp. 1,000,000",
 }
 
+const store9 = [
+    {   title:"Alamat", 
+        description : "Jl.Rawa Belong No.2 , Kebon Jeruk, Jakarta Barat"
+    },
+    {   title:"Kurikulum Sekolah", 
+        description : "International"
+    },
+    {   title:"Telepon", 
+        description : "021-26678321"
+    },
+    {   title:"Web", 
+        description : "www.smainternational.ac.id"
+    },
+    {   title:"Email", 
+        description : "smainternational@mail.ac.id"
+    },
+    {   title:"Web", 
+        description : "sdwidurijaya@blogspot.com"
+    },
+    {   title:"Jam Sekolah", 
+        description : "06:30 - 12:00"
+    },
+    {   title:"Kepala Sekolah", 
+        description : "Anita"
+    },
+    {   title:"Jumlah Siswa", 
+        description : "2000 Siswa"
+    }
+]
+
+
 class Detail extends Component {
     render() {
         return (
             <>
                 <div style={{overflow:"hidden"}}>
+                    <section>
+                        <div style={{marginTop: "18px"}} />
+                        <MobileDescription store={store9} />
+                    </section>
                     <section>
                         <div style={{marginTop: "18px"}} />
                         <CarouselMobile store={store} />
