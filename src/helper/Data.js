80f4d67b497de05d69eff141007835ef86760b4f
iipshoifuddin <shoifuddin.arkademy@gmail.com>
2020-04-15 08:33:18 +0700

    fourth commit

---
+++ src/helper/Data.js
@@ -0,0 +1,95 @@
+/*Untuk Card List Pencarian*/
+let store = [{
+        title   : "SMAN 14 JAKARTA JAKARTA JAKARTAJAKARTA",
+        address : "Jl. SMA BARAT, CILILITAN",
+        phone   : "021 - 525 8721"
+    },
+    {
+        title   : "SMAN 51 JAKARTA",
+        address : "JL. BATU AMPAR 3 CONDET",
+        phone   : "021 - 525 8721"
+    },
+    {
+        title   : "SMAN 62 JAKARTA",
+        address : "JL. RAYA BOGOR KM.20",
+        phone   : "021 - 525 8721"
+    },
+    {
+        title   : "SMAN 93 JAKARTA",
+        address : "JL. RAYA BOGOR KOMP. PASWALPRES",
+        phone   : "021 - 525 8721"
+    },
+];
+
+/*Untuk Carousel Images*/
+const store = [
+    {image : slideImage1},
+    {image : slideImage2},
+    {image : slideImage1},
+    {image : slideImage2}
+];
+
+/*Untuk Alamat Sekolah*/
+const store9 = [
+    {   title:"Alamat", 
+        description : "Jl.Rawa Belong No.2 , Kebon Jeruk, Jakarta Barat"
+    },
+    {   title:"Kurikulum Sekolah", 
+        description : "International"
+    },
+    {   title:"Telepon", 
+        description : "021-26678321"
+    },
+    {   title:"Web", 
+        description : "www.smainternational.ac.id"
+    },
+    {   title:"Email", 
+        description : "smainternational@mail.ac.id"
+    },
+    {   title:"Web", 
+        description : "sdwidurijaya@blogspot.com"
+    },
+    {   title:"Jam Sekolah", 
+        description : "06:30 - 12:00"
+    },
+    {   title:"Kepala Sekolah", 
+        description : "Anita"
+    },
+    {   title:"Jumlah Siswa", 
+        description : "2000 Siswa"
+    }
+];
+
+/*Untuk Ektrakulikuler atau Fasilitas dengan ICON*/
+const store6 = [
+    {iconname:"camera", name: "Fotografi", fontFamily : "FontAwesome"}, 
+    {iconname:"soccer-ball-o", name: "Futsal", fontFamily : "FontAwesome"},
+    {iconname:"dribbble", name: "BasketBall", fontFamily : "FontAwesome"}, 
+];
+
+/*Untuk Ektrakulikuler atau Fasilitas dengan check ICON*/
+const store7 =[
+    {name:"Orchestra"},
+    {name:"Paskibra"},
+    {name:"Tari Saman"}
+];
+
+/*Untuk MAP*/
+const store5 = {
+    center: {
+        lat: -6.2260798,
+        lng: 106.8536369
+    },
+    zoom: 15,
+    title:"Alamat Sekolah",
+    addr : "jl. sma barat no 4, clilitan, Kramat jati, jakarta timur"
+};
+
+/*Untuk Description*/
+const text = `SMA Negeri 14 Jakarta yang pada awalnya sekolah binaan khusus, kemudian berubah status menjadi salah
+erisque enim ligula 
+venenatis dolor. Maecenas nisl est, ultrices nec congue eget, auctor vitae massa. Fusce luctus vestibulum augue ut aliquet. 
+Nunc sagittis dictum nisi, sed ullamcorper ipsum dignissim ac. In at libero sed nunc venenatis imperdiet sed ornare turpis. 
+Donec vitae dui eget tellus gravida venenatis. 
+Integer fringilla congue eros non fermentum. Sed dapibus pulvinar nibh tempor porta.                 
+`;
