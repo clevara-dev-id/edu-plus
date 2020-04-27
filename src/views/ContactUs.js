dac53393cd28e2061732fae566524105864f0d19
iipshoifuddin <shoifuddin.arkademy@gmail.com>
2020-04-27 09:58:38 +0700

    second commit

--- src/views/ContactUs.js
+++ src/views/ContactUs.js
@@ -134,10 +134,6 @@ class ContactUs extends Component {
                                 onClickButton={()=>{console.log("Button Is Activated !")}}
                             />
                         </section>
-                        <section>
-                            <div style={{marginTop:"25px"}}></div>
-                            <CardListWatsApp />
-                        </section>
                     </OnMobile>
                 </div>
             </>
